bd8d3e908c202a01119da0c47e6b1cd871a4a5cd
OTI-AB <phillip.pearson@onsitetechnologies.caB>
2024-03-10 07:18:10 -0600

    Completed Stats w/ JS Increment

--- js/script.js
+++ js/script.js
@@ -11,4 +11,28 @@ function userScroll() {
 	});
 }
 
+function incrementStats() {
+	const countsArr = document.querySelectorAll('.counter'); //this is actually a node list but behaves like array
+
+	countsArr.forEach((counter) => {
+		counter.innerText = 0;
+
+		function updateCounter() {
+			const target = +counter.getAttribute('data-target');
+			const c = +counter.innerText;
+
+			const incr = target / 200;
+
+			if (c < target) {
+				counter.innerText = Math.ceil(c + incr);
+				setTimeout(updateCounter, 1);
+			} else {
+				counter.innerText = target;
+			}
+		}
+		updateCounter();
+	});
+}
+
 document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('DOMContentLoaded', incrementStats);
