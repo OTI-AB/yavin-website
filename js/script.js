c2ad71e0e183df900f2176ea86495edbd68e6c3f
OTI-AB <phillip.pearson@onsitetechnologies.caB>
2024-03-11 13:57:21 -0600

    Completecd Scroll to top btn

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 // Custom JavaScript
 function userScroll() {
 	const navbar = document.querySelector('.navbar');
+	const toTopBtn = document.getElementById('to-top');
 
 	window.addEventListener('scroll', () => {
 		if (window.scrollY > 50) {
 			navbar.classList.add('navbar-sticky');
+			toTopBtn.classList.add('show');
 		} else {
 			navbar.classList.remove('navbar-sticky');
+			toTopBtn.classList.remove('show');
 		}
 	});
 }
+
+const scrollToTop = () => {
+	document.documentElement.scrollTop = 0;
+};
+
+const scrollBtn = document
+	.getElementById('to-top')
+	.addEventListener('click', scrollToTop);
 
 function incrementStats() {
 	const countsArr = document.querySelectorAll('.counter'); //this is actually a node list but behaves like array
